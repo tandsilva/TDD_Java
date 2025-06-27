74dbc4fd3b6e73cd70bf75562647d354bcee7b3f
tandsilva <tamdsilva@logatti.edu.br>
2025-06-27 17:29:01 -0300

    Consultando CSV

--- src/test/java/math/ArraysCompareTest.java
+++ src/test/java/math/ArraysCompareTest.java
@@ -1,23 +1,42 @@
-package math;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.sql.Array;
-import java.util.Arrays;
-
-import org.junit.jupiter.api.Test;
-
-class ArraysCompareTest {
-
-	@Test
-	void test() {
-		int[] numbers = {25,8,21,32,3};
-		int[] ExpectedArray = {3,8,21,25,32};
-		Arrays.sort(numbers);
-		
-		
-		assertArrayEquals(numbers, ExpectedArray);
-		
-	}
-
-}
+//package math;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import java.sql.Array;
+//import java.util.Arrays;
+//import java.util.concurrent.TimeUnit;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Timeout;
+//
+//class ArraysCompareTest {
+//
+////	@Test
+////	void test() {
+////		int[] numbers = {25,8,21,32,3};
+////		int[] ExpectedArray = {3,8,21,25,32};
+////		Arrays.sort(numbers);
+////		
+////		
+////		assertArrayEquals(numbers, ExpectedArray);
+////		
+////	}
+//	
+//	@Test
+//	//@Timeout(1)
+//	@Timeout(value = 15,unit = TimeUnit.MILLISECONDS)
+//	void testPerformance() {
+//		int[] numbers = {25,8,21,32,3};
+//		for(int i=0;i<1000000;i++) {
+//			numbers[0]=i;
+//			Arrays.sort(numbers);
+//		}
+//	
+//		
+//	
+//		
+//	}
+//	
+//	
+//
+//}
